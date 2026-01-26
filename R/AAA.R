6507cb6461e501cd9a50c56eafbefa0cb35e670d
Kyle Baron <kyleb@metrumrg.com>
2026-01-26 07:44:48 -0600

    tweak imports

--- R/AAA.R
+++ R/AAA.R
@@ -1,16 +1,5 @@
-#' @importFrom fst read_fst write_fst
-#' @importFrom tools file_path_sans_ext file_ext
-#' @importFrom utils askYesNo
-#' @importFrom rlang abort warn
+#' @importFrom rlang abort warn caller_env
 NULL
 
 #' @keywords internal
 "_PACKAGE"
-
-.pkgenv <- new.env(parent = emptyenv())
-stream_types <- c("fst", "feather", "parquet", "qs", "rds")
-stream_format_classes <- paste0("stream_format_", stream_types)
-names(stream_format_classes) <- stream_types
-.pkgenv$stream_format_classes <- stream_format_classes
-.pkgenv$stream_types <- stream_types
-# nocov end
