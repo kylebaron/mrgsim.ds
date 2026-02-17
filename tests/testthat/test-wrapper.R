770039f2b337ca5d6ab62ce4e4cff00312859a0b
Kyle Baron <kyleb@metrumrg.com>
2026-02-16 23:10:12 -0600

    testing wrappers

---
+++ tests/testthat/test-wrapper.R
@@ -0,0 +1,26 @@
+library(testthat)
+library(mrgsim.ds)
+
+test_that("mrgsolve wrappers", {
+  x <- modlib_ds("pk1", param = list(CL = 2), compile = FALSE)
+  expect_is(x, "mrgmod")
+  expect_equal(x$CL, 2)
+  
+  code <- "$param a = 1"
+  x <- mcode_ds("example-mcode", code, end = 12, compile = FALSE)
+  expect_is(x, "mrgmod")
+  expect_equal(x$end, 12)
+  
+  tmp <- tempfile()
+  cat(code, file = tmp, sep = "\n")
+  x <- mread_ds(tmp, delta = 5, compile = FALSE)
+  expect_is(x, "mrgmod")
+  expect_equal(x$delta, 5)
+  
+  x <- mread_cache_ds(tmp, rtol = 1e-2, compile = FALSE)
+  expect_is(x, "mrgmod")
+  expect_equal(x$rtol, 1e-2)
+})
+
+
+
