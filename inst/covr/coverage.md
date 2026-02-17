185caabc57c0a0762fd6e202aff809aa508cf51a
Kyle Baron <kyleb@metrumrg.com>
2026-02-16 22:50:51 -0600

    files

--- inst/covr/coverage.md
+++ inst/covr/coverage.md
@@ -1,13 +1,13 @@
-# coverage: 71.38%
+# coverage: 83.33%
 
 |file          | coverage|
 |:-------------|--------:|
 |R/prune.R     |     0.00|
 |R/verbs.R     |     0.00|
 |R/wrapper.R   |    20.00|
-|R/files.R     |    45.88|
 |R/utils.R     |    50.00|
 |R/reduce.R    |    89.74|
+|R/files.R     |    90.59|
 |R/mrgsimsds.R |    95.60|
 |R/collect.R   |   100.00|
 |R/refresh.R   |   100.00|
--- tests/testthat/test-files.R
+++ tests/testthat/test-files.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(mrgsim.ds)
 
-mod <- house_ds()
+mod <- house_ds(end = 3)
 
 test_that("total size", {
   out <- mrgsim_ds(mod, gc = FALSE)
@@ -51,6 +51,44 @@ test_that("write_ds", {
   expect_false(x$gc)
   expect_equal(basename(x$files), "test-write")
   expect_equal(dirname(normalizePath(x$files)), normalizePath(tempdir()))
+})
+
+test_that("move_ds", {
+  out <- mrgsim_ds(mod)
+  nw <- file.path(tempdir(), "newdir")
+  x <- move_ds(out, path = nw)
+  tst <- basename(dirname(x$files))
+  expect_equal(tst, "newdir")
+  expect_true(x$gc)
+})
+
+test_that("temp file helpers", {
+  purge_temp()
+  out <- mrgsim_ds(mod, id = "AA1", gc = FALSE)
+  out <- mrgsim_ds(mod, id = "AA2", gc = FALSE)
+  out <- mrgsim_ds(mod, id = "AA3", gc = FALSE)
+  x <- suppressMessages(list_temp())
+  expect_length(x, 3)
+  expect_match(x, "mrgsims-ds-AA[0-9]")
+  expect_message(x <- purge_temp(), "Discarding 3 files.")
+  expect_null(x)
+  
+  suppressMessages(purge_temp())
+  out1 <- mrgsim_ds(mod, id = "AA1", gc = FALSE)
+  out2 <- mrgsim_ds(mod, id = "AA2", gc = FALSE)
+  out3 <- mrgsim_ds(mod, id = "AA3", gc = FALSE)
+  f <- c(out1$files, out3$files)  
+  expect_message(retain_temp(out1, out3), "Discarding 1 files.")
+  suppressMessages(x <- list_temp())
+  expect_identical(
+    normalizePath(f), 
+    normalizePath(x)
+  )
+  
+  suppressMessages(purge_temp())
+  out <- lapply(1:7, \(x) mrgsim_ds(mod))
+  suppressMessages(x <- list_temp())
+  expect_length(x, 7)
 })
 
 rm(mod)
